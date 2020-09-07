4d7588eb694a943f4470b2f64e43e392141effbc
Jeffrey Vermeire <jdvermeire@gmail.com>
2020-09-07 12:52:57 -0400

    first full iteration. it's a bit of a dirty cut, but it works.

--- dar-wip.py
+++ dar-wip.py
@@ -1,15 +1,23 @@
 import json
 # import ssl
 import asyncio
+import requests
 import datetime
 import websockets
 
 from pandas import DataFrame
 
 # TODO add logging
+# TODO add exchange rate from: https://api.exchangeratesapi.io/latest?symbols=USD
 
 
 ## MOCK ENVIRONMENT VARIABLES ##
+
+# uri for EUR exchange rates
+MOCK_EV_EXCHANGE_URI = 'https://api.exchangeratesapi.io/latest'
+
+# list of currencies to return EUR exchange rate
+MOCK_EV_EXCHANGE_SYMBOLS = ['USD']
 
 # base uri to connect to coinbase pro websocket
 # MOCK_EV_URI = 'wss://ws-feed-public.sandbox.pro.coinbase.com'
@@ -19,13 +27,16 @@ MOCK_EV_URI = 'wss://ws-feed.pro.coinbase.com'
 MOCK_EV_PRODUCTS = ["BTC-USD", "BTC-EUR"]
 
 # interval, in seconds, to send data to CSV
-MOCK_EV_EXPORT_INTERVAL = 600
+MOCK_EV_EXPORT_INTERVAL = 20
 
 # interval, in seconds, to run app
 MOCK_EV_RUN_INTERVAL = 120
 
 # inverval, in seconds, to aggregate volume weighted average
 MOCK_EV_VWA_INTERVAL = 60
+
+# timestamp format string
+MOCK_EV_TIMESTAMP_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 
 # directory to dump files
 MOCK_EV_DUMP_PATH = 'data/'
@@ -43,20 +54,11 @@ class Coinbase(object):
 	def __init__(
 		self,
 		websocket,
-		export_seconds,
-		run_seconds,
-		vwa_seconds,
 		*args, 
 		**kwargs
 	):
 		# TODO clean up unnecessary params
 		self.ws = websocket
-		self.start_time = datetime.datetime.now()
-		self.run_interval = datetime.timedelta(seconds=run_seconds)
-		self.export_interval = datetime.timedelta(seconds=export_seconds)
-		self.vwa_interval = datetime.timedelta(seconds=vwa_seconds)
-		self.end_time = self.start_time + self.run_interval
-		self.next_dump_time = self.start_time + self.export_interval
 
 
 	async def subscribe(self, products, channels):
@@ -171,8 +173,8 @@ class Coinbase(object):
 			a heartbeat message from a websocket connection
 		"""
 		# DEBUG
-		# print(message)
-		pass
+		print(message)
+		# pass
 
 
 	async def snapshot_handler(self, message):
@@ -254,6 +256,7 @@ class Coinbase(object):
 
 	async def match_handler(self, message):
 		"""
+		Handle match type messages from coinbase websocket channel
 
 		Parameters
 		----------
@@ -320,53 +323,46 @@ class Coinbase(object):
 		self.queues[queue_name] = []
 		
 
+# -------------------------------------------------------------------
 
-async def run():
-	
-	uri = MOCK_EV_URI
 
-	print("connecting to coinbase websocket...")
+def get_eur_rates(symbols):
+	"""
+	Return a dict of EUR exchange rates for the input currency symbols
 
-	# TODO add error handling here with a try/catch block
-	#      allow for websocket to reconnect on timeout
-	async with websockets.connect(uri) as ws:
-		print("connected.")
+	Parameters
+	----------
+	symbols : list
+		a list of currency symbols
+	"""
 
-		ch = Coinbase(
-			websocket = ws,
-			# TODO remove unnecessary params
-			run_seconds=MOCK_EV_RUN_INTERVAL,
-			export_seconds=MOCK_EV_EXPORT_INTERVAL,
-			vwa_seconds=MOCK_EV_VWA_INTERVAL,
-			dump_path=MOCK_EV_DUMP_PATH,
-		)
+	resp = requests.get(
+		MOCK_EV_EXCHANGE_URI,
+		params={
+			"symbols": ','.join(symbols)
+		}
+	)
 
-		# using the "matches" channel as it streams completed portions of orders
-		await ch.subscribe(products=MOCK_EV_PRODUCTS, channels=["heartbeat", "matches"])
-		
-		while datetime.datetime.now() < ch.end_time:
-			
-			resp = await ws.recv()
+	return resp.json()["rates"]
 
-			await ch.message_handler(json.loads(resp))
 
-		print('closing...')
-
-	df = DataFrame(ch.queues["match"])
-	df = calculate_table(df)
-	print(df.columns)
-	print(df.dtypes)
-	print(df)
-	# print(ch.ticker_queue)
-
+# -------------------------------------------------------------------
 
 def calculate_table(match_queue_df):
 	"""
 	Return table with minute-by-minute volume weighted average price
+
+	Parameters
+	----------
+	match_queue_df : DataFrame
+		a DataFrame of the match queue from coinbase
 	"""
+	# get the current exchange rate for USD
+	eur_usd = get_eur_rates(["USD"])["USD"]
+
 	df = DataFrame()
 	df["product"] = match_queue_df["product_id"]
-	df["time"] = match_queue_df["time"].apply(lambda dt: datetime.datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S.%fZ'))
+	df["time"] = match_queue_df["time"].apply(lambda dt: datetime.datetime.strptime(dt, MOCK_EV_TIMESTAMP_FORMAT))
 
 	# truncate time to minute
 	df["time"] = df["time"].apply(lambda dt: datetime.datetime(
@@ -378,6 +374,10 @@ def calculate_table(match_queue_df):
 	))
 	df["price"] = match_queue_df["price"].apply(lambda prc: float(prc))
 	df["size"] = match_queue_df["size"].apply(lambda sz: float(sz))
+
+	# convert EUR to USD
+	df.loc[df["product"] == 'BTC-EUR', ["price"]] *= eur_usd
+
 	df["volume_price"] = df["price"] * df["size"]
 
 	df = df.groupby(by=["product", "time"]).sum()
@@ -388,6 +388,117 @@ def calculate_table(match_queue_df):
 
 	return df
 
+# -------------------------------------------------------------------
+
+def dump_vwa(cb, path='', as_of=datetime.datetime.now()):
+	"""
+	Cuts and dumps the volume weighted average price from a coinbase match queue
+
+	Parameters
+	----------
+	cb : Coinbase
+		Coinbase object that holds the match queue
+	path : str
+		Directory to save export file
+	as_of : datetime
+		Datetime at which to cut the match queue
+	"""
+	if path.endswith('/'):
+		path = path.rstrip('/')
+
+	print('length of match queue: %s' % len(cb.queues["match"]))
+	print(datetime.datetime.strptime(cb.queues["match"][0]["time"], MOCK_EV_TIMESTAMP_FORMAT))
+	print(as_of)
+
+	cut_queue = [
+		rec 
+		for rec 
+		in cb.queues["match"] 
+		if datetime.datetime.strptime(rec["time"], MOCK_EV_TIMESTAMP_FORMAT) <= as_of
+	]
+
+	print('length of cut queue: %s' % len(cut_queue))
+
+	if not len(cut_queue):
+		return
+
+	df = DataFrame(cut_queue)
+
+	# get calculated dataframe to export
+	df = calculate_table(df)
+
+	df.to_csv('%s/match_%s.csv' % (path, as_of.strftime("%Y%m%d%H%M%S")))
+
+	# reset queue
+	# TODO there's likely a more performant way to do this, but this works for now
+	cb.queues["match"] = [
+		rec
+		for rec
+		in cb.queues["match"]
+		if datetime.datetime.strptime(rec["time"], MOCK_EV_TIMESTAMP_FORMAT) > as_of
+	]
+
+# -------------------------------------------------------------------
+
+async def run():
+	
+	# get environment vars
+	uri = MOCK_EV_URI
+	
+	exchange_uri = MOCK_EV_EXCHANGE_URI
+	exchange_symbols = MOCK_EV_EXCHANGE_SYMBOLS
+
+	run_seconds = MOCK_EV_RUN_INTERVAL
+	export_seconds = MOCK_EV_EXPORT_INTERVAL
+	# vwa_seconds = MOCK_EV_VWA_INTERVAL	# Not going to use this here, not sure on the implementation
+	dump_path = MOCK_EV_DUMP_PATH
+
+	# initial run vars
+	start_time = datetime.datetime.utcnow()
+	run_interval = datetime.timedelta(seconds=run_seconds)
+	export_interval = datetime.timedelta(seconds=export_seconds)
+	# vwa_interval = datetime.timedelta(seconds=vwa_seconds)
+	end_time = start_time + run_interval
+	next_dump_time = datetime.datetime(
+		year=start_time.year,
+		month=start_time.month,
+		day=start_time.day,
+		hour=start_time.hour,
+		minute=start_time.minute) + export_interval
+
+	print("connecting to coinbase websocket...")
+
+	# TODO add error handling here with a try/catch block
+	#      allow for websocket to reconnect on timeout
+	async with websockets.connect(uri) as ws:
+		print("connected.")
+
+		ch = Coinbase(
+			websocket = ws,
+		)
+
+		# using the "matches" channel as it streams completed portions of orders
+		await ch.subscribe(products=MOCK_EV_PRODUCTS, channels=["heartbeat", "matches"])
+		
+		while datetime.datetime.utcnow() < end_time:
+
+			resp = await ws.recv()
+
+			await ch.message_handler(json.loads(resp))
+
+			if datetime.datetime.utcnow() > next_dump_time:
+				dump_vwa(ch, path=dump_path, as_of=next_dump_time)
+				print('current dump time: %s' % next_dump_time)
+
+				next_dump_time += export_interval
+
+				print('next dump time: %s' % next_dump_time)
+
+		print('closing...')
+
+		# dump remainder of queue
+		if len(ch.queues["match"]):
+			dump_vwa(ch, path=dump_path)
 
 
 asyncio.get_event_loop().run_until_complete(run())
